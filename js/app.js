1776b6b21e9c329355cdfea740ac038fbb584302
Hannah Tucker McLellan <hannahjt.703@gmail.com>
2018-02-17 23:21:56 +0000

    created the first few views, need to fix the chevron arrows (which were working half an hour ago), and next step is to work on carousel

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('js is loaded');
+
+$(() => {
+  // on page load, show main div;
+  $('.page-one').show();
+
+  // hide and show pages;
+  // page one
+  $('.home').on('click', function() {
+    $('.page-one').show();
+    $('.page-two').hide();
+    $('.page-three').hide();
+    $('.page-four').hide();
+    $('.page-five').hide();
+  });
+  // page two
+  $('.projects').on('click', function() {
+    $('.page-one').hide();
+    $('.page-two').show();
+    $('.page-three').hide();
+    $('.page-four').hide();
+    $('.page-five').hide();
+  });
+  // page two project carousel
+
+  // page three
+  $('.skills').on('click', function() {
+    $('.page-one').hide();
+    $('.page-two').hide();
+    $('.page-three').show();
+    $('.page-four').hide();
+    $('.page-five').hide();
+  });
+
+
+
+// window load function ends
+});
