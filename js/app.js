d21439f78e6c12e7741682d3a3409eb8fd7d569e
Hannah Tucker McLellan <hannahjt.703@gmail.com>
2018-02-24 14:04:27 +0000

    changing the layout and look

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-$(main);
-
-function navListener() {
-  const menuNodes = document.querySelectorAll('#menu > li > a');
-  // make an array with the targets from the links in our #menu
-  const allNavs = [...menuNodes].map(el => el.dataset.target)
-  // when a link in our menu is clicked, show the target
-  // hide the others
-  $('#menu').on('click', 'a', function(e) {
-    const target = e.target.dataset.target;
-    const others = allNavs.filter(el => el != target);
-    $(`.page-${target}`).show();
-    others.forEach(other => $(`.page-${other}`).hide());
-  })
-}
-
-function main() {
-  // carousel
-  // $('.slick-carousel').slick();
-  //show main div;
-  $('.page-home').show();
-  navListener();
-}
+// $(main);
+//
+// function navListener() {
+//   const menuNodes = document.querySelectorAll('#menu > li > a');
+//   // make an array with the targets from the links in our #menu
+//   const allNavs = [...menuNodes].map(el => el.dataset.target)
+//   // when a link in our menu is clicked, show the target
+//   // hide the others
+//   $('#menu').on('click', 'a', function(e) {
+//     const target = e.target.dataset.target;
+//     const others = allNavs.filter(el => el != target);
+//     $(`.page-${target}`).show();
+//     others.forEach(other => $(`.page-${other}`).hide());
+//   })
+// }
+//
+// function main() {
+//   // carousel
+//   // $('.slick-carousel').slick();
+//   //show main div;
+//   $('.page-home').show();
+//   navListener();
+// }
